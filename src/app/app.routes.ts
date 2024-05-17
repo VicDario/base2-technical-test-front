a6653522662a0d940968cb07b825af93700acee1
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-17 15:24:01 -0400

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+          },
+          {
+            path: 'products',
+            loadComponent: () => import('./pages/products/products.component').then(c => c.ProductsComponent),
+          },
+          {
+            path: 'categories',
+            loadComponent: () => import('./pages/categories/categories.component').then(c => c.CategoriesComponent),
+          }
+      ]
+    }
+];
