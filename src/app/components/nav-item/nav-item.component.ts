9acf2e84f253d585c08341c7fe2e1207e4b8cd20
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-17 19:45:49 -0400

    feat: nav-item component

---
+++ src/app/components/nav-item/nav-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-nav-item',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './nav-item.component.html',
+  styleUrl: './nav-item.component.scss'
+})
+export class NavItemComponent {
+  @Input({ required: true }) navData!: NavItem;
+  public faChevronRight = faChevronRight;
+}
+
+export interface NavItem {
+  name: string;
+  route: string;
+  icon: IconDefinition;
+}
+
