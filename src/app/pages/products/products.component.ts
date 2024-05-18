b2a1cd1cb3e82a643af85fcea32ae99926069b09
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-17 22:55:45 -0400

    feat: init product page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TableComponent } from '@/components/table/table.component';
+import { ProductsService } from '@/services/products.service';
+import { Products } from '@/models/products.model';
+import { Pagination } from '@/models/query.model';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  private readonly productsService = inject(ProductsService);
+
+  columns: string[] = ['name','brand', 'price', 'stock', 'category'];
+  products: Products[] = [];
+  pagination: Pagination = {
+    offset: 0,
+    limit: 10,
+  }
 
 }
