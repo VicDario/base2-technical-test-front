29aaf6c90f60caaaca14cf773eb6e9fac2058d72
Víctor Fernández <26585356+VicDario@users.noreply.github.com>
2024-05-18 07:06:30 +0000

    fix: avoid reload when navigate

--- src/app/components/nav-item/nav-item.component.ts
+++ src/app/components/nav-item/nav-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
@@ -6,7 +7,7 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-nav-item',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './nav-item.component.html',
   styleUrl: './nav-item.component.scss'
 })
