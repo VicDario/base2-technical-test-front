b2cc1c94ef9fbfb06bd11317131c84bee5c6d5be
Víctor Fernández <26585356+VicDario@users.noreply.github.com>
2024-05-18 07:53:32 +0000

    feat: show table with products data and pagination

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,24 +1,56 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { TableComponent } from '@/components/table/table.component';
-import { ProductsService } from '@/services/products.service';
-import { Products } from '@/models/products.model';
+import { ProductsService } from '@/services/products/products.service';
+import { Product } from '@/models/product.model';
 import { Pagination } from '@/models/query.model';
+import { map } from 'rxjs';
+import { Category } from '@/models/category.model';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [TableComponent],
+  imports: [TableComponent, MatPaginatorModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   private readonly productsService = inject(ProductsService);
 
-  columns: string[] = ['name','brand', 'price', 'stock', 'category'];
-  products: Products[] = [];
+  columns: string[] = ['sku', 'name', 'brand', 'price', 'stock', 'category'];
+  products: (Product & { category: string })[] = [];
   pagination: Pagination = {
     offset: 0,
     limit: 10,
+  };
+  totalItems = 0;
+
+  ngOnInit() {
+    this.getProducts();
   }
 
+  private getProducts() {
+    this.productsService
+    .getProducts(this.pagination)
+    .pipe(
+      map(
+        (result) => ({
+          products: result.products.map((product) => ({...product, category: (product.category as Category).name})),
+          total: result.total,
+        })
+      )
+    )
+    .subscribe({
+      next: (result) => {
+        this.products = result.products;
+        this.totalItems = result.total;
+      },
+    });
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.pagination.offset = (event.pageIndex + 1) * event.pageSize;
+    this.pagination.limit = event.pageSize;
+    this.getProducts();
+  }
 }
