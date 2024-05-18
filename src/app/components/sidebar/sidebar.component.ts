7ed61ee3466a75dec4a0b42d90049abe303e0429
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-17 20:02:45 -0400

    feat: sidebar component completed

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
+import { faTags, faStore } from '@fortawesome/free-solid-svg-icons';
+import { NavItem, NavItemComponent } from '../nav-item/nav-item.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, RouterModule],
+  imports: [MatSidenavModule, RouterModule, NavItemComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  public faTags = faTags;
+  public faStore = faStore;
 
+  public navItems: NavItem[] = [
+    {
+      name: 'Products',
+      route: '/products',
+      icon: this.faStore
+    },
+    {
+      name: 'Categories',
+      route: '/categories',
+      icon: this.faTags
+    }
+  ]
 }
+
