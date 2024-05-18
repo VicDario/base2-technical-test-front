2fe4ec55e3c4d391aa18171bd83e0ff22749d4e4
Víctor Fernández <26585356+VicDario@users.noreply.github.com>
2024-05-18 08:09:59 +0000

    feat: add category result interface

--- src/app/models/response.model.ts
+++ src/app/models/response.model.ts
@@ -1,6 +1,12 @@
+import { Category } from './category.model';
 import { Product } from './product.model';
 
 export interface ProductsResult {
   products: Product[];
+  total: number;
+}
+
+export interface CategoriesResult {
+  categories: Category[];
   total: number;
 }
