11d3ba06c5078e1ef38ddc69923f0d88473f90ca
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-17 22:55:25 -0400

    feat: add declarations of some methods

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = environment.apiUrl;
 
-  constructor() { }
+  getProducts() {}
+
+  getProduct(id: string) {}
+
+  createProduct(product: any) {}
 }
